24a09081370dec9eaff6ce805596676150163516
akmalsyrf <dev.akmal69@gmail.com>
2024-02-20 15:26:13 +0700

    check member by user_id

--- bot.js
+++ bot.js
@@ -13,38 +13,48 @@ bot.getMe().then((user) => {
 });
 
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
-
-  if (text === '/start') {
-    bot.sendMessage(chatId, 'Welcome to the bot!');
-  }
-
-  if (text === '/check') {
-    conversationStatus[chatId]['status'] = 'waitingForPhoneNumber';
-    conversationStatus[chatId]['user_id'] = msg.from.id
-    bot.sendMessage(chatId, 'Masukkan nomor telepon:');
-  }
-
-  if (
-    conversationStatus[chatId]
-    && conversationStatus[chatId].status === 'waitingForPhoneNumber'
-    && conversationStatus[chatId].user_id === msg.from.id
-    ) {
-    const phoneNumber = msg.text;
-
-    delete conversationStatus[chatId];
-
-    bot.getChatMember(chatId, phoneNumber)
-      .then((member) => {
-        if (member.status === 'member') {
-          bot.sendMessage(chatId, `Nomor ${phoneNumber} mengikuti grup ini.`);
-        } else {
-          bot.sendMessage(chatId, `Nomor ${phoneNumber} tidak mengikuti grup ini.`);
+    try {
+        const chatId = msg.chat.id;
+        const text = msg.text;
+      
+        if (text === '/start') {
+          bot.sendMessage(chatId, 'Welcome to the bot!');
         }
-      })
-      .catch((error) => {
-        bot.sendMessage(chatId, `Error: ${error}`);
-      });
-  }
-});
+      
+        if (text === '/check') {
+          conversationStatus[chatId] = {
+            status: 'waitingForPhoneNumber',
+            user_id: msg.from.id
+          }
+          bot.sendMessage(chatId, 'Masukkan link user yang ingin di cek apakah dia anggota grup ini atau bukan:');
+        }
+      
+        if (
+          conversationStatus[chatId] && msg.reply_to_message
+          && conversationStatus[chatId].status === 'waitingForPhoneNumber'
+          && conversationStatus[chatId].user_id === msg.from.id
+          ) {
+      
+          delete conversationStatus[chatId];
+
+          const regex = /a\/#(\d+)/;
+          const match = regex.exec(msg.text); // example: https://web.telegram.org/a/#-4138651925
+          const user_id = match[1]
+      
+          bot.getChatMember(chatId, user_id)
+            .then((member) => {
+                console.log('member ', member)
+              if (member.status === 'member') {
+                bot.sendMessage(chatId, `Id ${user_id} mengikuti grup ini.`);
+              } else {
+                bot.sendMessage(chatId, `Id ${user_id} tidak mengikuti grup ini.`);
+              }
+            })
+            .catch((error) => {
+              bot.sendMessage(chatId, `Error: ${error}`);
+            });
+        }
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+    }
+})
